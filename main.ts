7b93b757581fafe6c04f90959450b60f22e63796
MINT Genie <66748747+MINTGenie@users.noreply.github.com>
2020-07-10 02:43:15 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+SuperBit.MotorStopAll()
+let lhand_pos = 70
+let rhand_pos = 50
+SuperBit.MotorRunDual(
+SuperBit.enMotors.M1,
+40,
+SuperBit.enMotors.M3,
+40
+)
+basic.forever(function () {
+    for (let index = 0; index < 12; index++) {
+        SuperBit.Servo(SuperBit.enServo.S1, rhand_pos)
+        rhand_pos += 2
+        basic.pause(50)
+    }
+    rhand_pos = 50
+    SuperBit.Servo(SuperBit.enServo.S1, rhand_pos)
+})
+basic.forever(function () {
+    for (let index = 0; index < 17; index++) {
+        SuperBit.Servo(SuperBit.enServo.S5, lhand_pos)
+        lhand_pos += 4
+        basic.pause(40)
+    }
+    for (let index = 0; index < 17; index++) {
+        SuperBit.Servo(SuperBit.enServo.S5, lhand_pos)
+        lhand_pos += -4
+        basic.pause(40)
+    }
+})
